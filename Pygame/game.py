dbdb22809186c4dd4dbeacc2029dbe23b211f019
viniciusroland <vini_roland4.10@hotmail.com>
2018-12-01 12:24:08 -0200

    POO adicionada

--- Pygame/game.py
+++ Pygame/game.py
@@ -2,17 +2,6 @@ import pygame
 import random
 
 pygame.init()
-
-rock = pygame.image.load('Game/rock_reduzida2.png')
-
-walkRight = [pygame.image.load('Game/R1.png'), pygame.image.load('Game/R2.png'), pygame.image.load('Game/R3.png'), pygame.image.load('Game/R4.png'), pygame.image.load('Game/R5.png'), pygame.image.load('Game/R6.png'), pygame.image.load('Game/R7.png'), pygame.image.load('Game/R8.png'), pygame.image.load('Game/R9.png')]
-
-walkLeft = [pygame.image.load('Game/L1.png'), pygame.image.load('Game/L2.png'), pygame.image.load('Game/L3.png'), pygame.image.load('Game/L4.png'), pygame.image.load('Game/L5.png'), pygame.image.load('Game/L6.png'), pygame.image.load('Game/L7.png'), pygame.image.load('Game/L8.png'), pygame.image.load('Game/L9.png')]
-
-bg = pygame.image.load('Game/bg.jpg') 
-
-char = pygame.image.load('Game/standing.png')
-
 w_win = 800
 h_win = 450
 win = pygame.display.set_mode((w_win, h_win))
@@ -20,60 +9,81 @@ win = pygame.display.set_mode((w_win, h_win))
 pygame.display.set_caption("Gamezinho")
 clock = pygame.time.Clock()
 
-isJump = False
+bg = pygame.image.load('Game/bg.jpg') 
 
-x = 50
-y = 360
-w = 64
-h = 64
-vel = 15
-left = False
-right = False
-walkCount = 0
-jumpCount = 10
-run = True
-x_rock = 700
-y_rock = 380
-vel_rock = 10
+class Player:
+    def __init__(self, x, y, w, h):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.vel = 15
+        self.left = False
+        self.right = False
+        self.walkCount = 0
+        self.jumpCount = 10
+        self.isJump = False        
+        self.walkRight = [pygame.image.load('Game/R1.png'), pygame.image.load('Game/R2.png'), pygame.image.load('Game/R3.png'), pygame.image.load('Game/R4.png'), pygame.image.load('Game/R5.png'), pygame.image.load('Game/R6.png'), pygame.image.load('Game/R7.png'), pygame.image.load('Game/R8.png'), pygame.image.load('Game/R9.png')]
+        
+        self.walkLeft = [pygame.image.load('Game/L1.png'), pygame.image.load('Game/L2.png'), pygame.image.load('Game/L3.png'), pygame.image.load('Game/L4.png'), pygame.image.load('Game/L5.png'), pygame.image.load('Game/L6.png'), pygame.image.load('Game/L7.png'), pygame.image.load('Game/L8.png'), pygame.image.load('Game/L9.png')]
+    
+        self.char = pygame.image.load('Game/standing.png')
+
+
+    def __str__(self):
+        return 'Player1'
+    
+    def drawPlayer(self):
+        if self.walkCount + 1 >= 27:
+            self.walkCount = 0
+
+        if self.left:
+            win.blit(self.walkLeft[self.walkCount//3] , (self.x, self.y))
+            self.walkCount += 1
+
+        elif self.right:
+            win.blit(self.walkRight[self.walkCount//3], (self.x, self.y))
+            self.walkCount += 1
+        else:
+            win.blit(self.char, (self.x, self.y))
+
+        pygame.display.update()
+
+
+    def atualizaPos(self, eixo, direcao):
+        if eixo == 'x':
+            self.x += self.vel * direcao
+
+        if eixo == 'y':
+            self.y += self.vel * direcao
+
+
+class Rock:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.img = pygame.image.load('Game/rock_reduzida2.png') 
+        self.vel_rock = 10 
+
+    def drawRock(self):
+        if self.x < 0:
+            self.vel_rock = random.randint(10, 40)
+            self.x = w_win
+        self.x -= self.vel_rock
+        win.blit(bg, (0, 0))
+        win.blit(self.img, (self.x, self.y))
+    
+player = Player(50, 360, 64, 64)
+rock = Rock(700, 380)
 pontuacao = 0
-
-
+run = True
 
 def calcula_pontos():
     global pontuacao
-    if(x == x_rock and y < y_rock):
+    if(player.x == rock.x and player.y < rock.y):
         pontuacao += 1
     
     print('Minha pontuacao: %s' % pontuacao)
-
-
-
-def draw():
-    global vel_rock
-    global x_rock
-    if x_rock < 0:
-        vel_rock = random.randint(10, 40)
-        x_rock = w_win
-    x_rock -= vel_rock
-    global walkCount
-    win.blit(bg, (0, 0))
-    win.blit(rock, (x_rock, y_rock))
-    
-    
-    if walkCount + 1 >= 27:
-        walkCount = 0
-
-    if left:
-        win.blit(walkLeft[walkCount//3], (x, y))
-        walkCount += 1
-
-    elif right:
-        win.blit(walkRight[walkCount//3], (x, y))
-        walkCount += 1
-    else:
-        win.blit(char, (x, y))
-
-    pygame.display.update()
 
 while run:
     clock.tick(27)
@@ -83,48 +93,52 @@ while run:
     
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
-        left = True
-        right = False
-        x = x - vel
-    elif keys[pygame.K_RIGHT]:
-        left = False
-        right = True
-        x = x + vel
-    else:
-        left = False
-        right = False
-        walkCount = 0
 
-    if not(isJump):
+        player.left = True
+        player.right = False
+        player.atualizaPos('x', -1)
+    elif keys[pygame.K_RIGHT]:
+
+        player.left = False
+        player.right = True
+        player.atualizaPos('x', 1)
+    else:
+
+        player.left = False
+        player.right = False
+        player.walkCount = 0
+
+    if not(player.isJump):
 
         if keys[pygame.K_SPACE]:
-            right = False
-            left = False
-            walkCount = 0
-            isJump = True
-
+            
+            player.right = False
+            player.left = False
+            player.walkCount = 0
+            player.isJump = True
     else:
-        if jumpCount >= -10:
+        if player.jumpCount >= -10:
             neg = 1
-            if jumpCount < 0:
+            if player.jumpCount < 0:
                 neg = -1
-            y -= (jumpCount ** 2) * 0.5 * neg
-            jumpCount -= 1
+            player.y -= (player.jumpCount ** 2) * 0.5 * neg
+            player.jumpCount -= 1
         else:
-            isJump = False
-            jumpCount = 10
+            player.isJump = False
+            player.jumpCount = 10
 
-    if(x > w_win):
-        x = 0
-    if(x < 0):
-        x = w_win
+    if(player.x > w_win):
+        player.x = 0
+    if(player.x < 0):
+        player.x = w_win
 
-    if(y > h_win):
-        y = 0
-    if(y < 0):
-        y = h_win
+    if(player.y > h_win):
+        player.y = 0
+    if(player.y < 0):
+        player.y = h_win
     
-    draw()    
+    player.drawPlayer()    
+    rock.drawRock()
     calcula_pontos()
 
 pygame.quit()
